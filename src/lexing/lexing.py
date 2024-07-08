a88542e0eeea9b8d1e98ddb30fd73e26c6d49ae5
Jay Choy <zcabzjc@ucl.ac.uk>
2024-07-08 21:41:05 +0100

    init with plz

---
+++ src/lexing/lexing.py
@@ -0,0 +1,10 @@
+import ast
+import logging
+
+class FunctionRules:
+    def __init__(self):
+        self.functions = []
+
+    def hello_world(self):
+        print("hello world")
+
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.lexing import lexing
+
+print("hello world")
+lexing.FunctionRules().hello_world()
